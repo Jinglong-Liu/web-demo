72ab44d512690b3012247255fdd545551866493c
Jinglong-Liu <1601029287@qq.com>
2024-06-22 12:58:54 +0800

    add web demo

--- README.md
+++ README.md
@@ -1,2 +1,6 @@
 # web-demo
 A demo of web application
+
+mvn clean install
+
+web-demo.war
---
+++ src/main/java/com/github/ljl/servlet/web/IndexServlet.java
@@ -0,0 +1,30 @@
+package com.github.ljl.servlet.web;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.io.PrintWriter;
+
+/**
+ * @program: web-demo
+ * @description:
+ * @author: ljl
+ * @create: 2024-06-22 12:51
+ **/
+
+public class IndexServlet extends HttpServlet {
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        resp.setContentType("text/html");
+        PrintWriter out = resp.getWriter();
+        out.println("<h1>servlet index get</h1>");
+    }
+    @Override
+    protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        resp.setContentType("text/html");
+        PrintWriter out = resp.getWriter();
+        out.println("<h1>servlet index post</h1>");
+    }
+}
