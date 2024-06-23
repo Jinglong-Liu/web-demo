4c0eed615170ee3a0c865304c7e97ed50ff65668
Jinglong-Liu <1601029287@qq.com>
2024-06-23 11:22:58 +0800

    [web-demo] v0.5.0 load-other-webapp

--- src/main/java/com/github/ljl/servlet/web/IndexServlet.java
+++ src/main/java/com/github/ljl/servlet/web/IndexServlet.java
@@ -5,12 +5,13 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
-import java.io.PrintWriter;
+import java.lang.reflect.Method;
 
 /**
  * @program: web-demo
  * @description:
  * @author: ljl
+ * @since 0.5.0
  * @create: 2024-06-22 12:51
  **/
 
@@ -18,13 +19,28 @@ public class IndexServlet extends HttpServlet {
     @Override
     protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
         resp.setContentType("text/html");
-        PrintWriter out = resp.getWriter();
-        out.println("<h1>servlet index get</h1>");
+        writeToResponse(resp,"servlet index get");
     }
     @Override
     protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
         resp.setContentType("text/html");
-        PrintWriter out = resp.getWriter();
-        out.println("<h1>servlet index post</h1>");
+        writeToResponse(resp,"servlet index post");
+    }
+    private void writeToResponse(HttpServletResponse resp, String content) {
+        try {
+            Class<?> responseClass = resp.getClass();
+            Method writeMethod = responseClass.getMethod("write", String.class);
+            writeMethod.invoke(resp, IndexServlet.http200Resp(content));
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+    public static String http200Resp(String rawText) {
+        String format = "HTTP/1.1 200 OK\r\n" +
+                "Content-Type: text/plain\r\n" +
+                "\r\n" +
+                "%s";
+
+        return String.format(format, rawText);
     }
 }
